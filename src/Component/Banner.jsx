bce640164d7792cf173a8c3b369039fcdc11f171
GoDofThunder9 <123013319+GoDofThunder9@users.noreply.github.com>
2024-12-08 11:30:29 +0530

    done with the setup

---
+++ src/Component/Banner.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../assets/Style/banner.css';
+import banner from '../assets/Images/Images/banner.svg'
+import Form from './form';
+function Banner(){
+  return (
+    <div className='banner'>
+      <>
+      <div>
+      <div className='FORM'>
+        <Form/>
+      </div>
+       <img src={banner} alt="cantLoad"  />
+       <div className='tag'>
+       Consultation,
+       Design,& Marketing
+       </div>
+      </div>
+     
+      </>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import  '../assets/Style/Navbar.css';
+import Logo from '../assets/Images/Images/logo.svg'
+import Banner from './Banner';
+import Intro from './Intro';
+import Aboutus from './aboutus';
+import OurProjectComponent from './Project';
+function Navbar() {
+  return (
+    <>
+    <div className='Navbar'>
+       <div className='img1'> <img src={Logo}  alt="not loaded" /></div>
+       <div className='Navsection'>
+         <a href="#">HOME</a>
+         <a href="#">SERVICES</a>
+         <a href="#">ABOUT PROJECT</a>
+         <a href="#">TESTIMONIAL</a>
+       </div>
+       <button className='Button1'> CONTACT</button>
+    </div>
+    <Banner/>
+    <Intro/>
+    <Aboutus/>
+    <OurProjectComponent/>
+    </>
+  )
+}
+
+export default Navbar
